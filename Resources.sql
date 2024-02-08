7810bba2162c65c126acdcd67d99ee36fbf9f7c1
swathi <157112581+swathichandrababu@users.noreply.github.com>
2024-02-08 22:43:12 +0000

    added product line name

--- Resources.sql
+++ Resources.sql
@@ -10,26 +10,43 @@ Tables used:
 twinkl.twinkl_resource_extra
 twinkl.twinkl_resource
 dx_resource
+twinkl_resource_product_line
+twinkl_product_line
 
 */
 
-SELECT re.resource_id,
-       r.title,
-       CONCAT('https://www.twinkl.com/resource/', r.friendly_url) AS url,
-       r.redirect_url
-FROM twinkl.twinkl_resource_extra AS re
-JOIN twinkl.twinkl_resource AS r ON re.resource_id = r.id
+SELECT
+    re.resource_id,
+    r.title,
+    CONCAT('https://www.twinkl.com/resource/', r.friendly_url) AS url,
+    r.redirect_url,
+    pl.name AS product_line_name, 
+    pl.external_name AS product_line_external_name
+FROM
+    twinkl.twinkl_resource_extra AS re
+JOIN
+    twinkl.twinkl_resource AS r ON re.resource_id = r.id
 LEFT JOIN (
-    SELECT resource_id,
-           bundle_id,
-           resource_package
-    FROM dx_resource
+    SELECT
+        resource_id,
+        bundle_id,
+        resource_package
+    FROM
+        dx_resource
 ) AS dx ON re.resource_id = dx.resource_id
-WHERE re.`key` = 'free_logged_out' AND re.value = 1 -- free_logged_out is 1
-  AND re.resource_id NOT IN ( -- selecting resources where free_logged in is either 0 or doesn't exist
-    SELECT resource_id
-    FROM twinkl.twinkl_resource_extra
-    WHERE `key` = 'free_logged_in' AND value = 1
-  )
-	AND dx.bundle_id IS NOT NULL -- resource is part of a bundle
-	AND r.approved = 1; -- only looking at approved resources
+LEFT JOIN
+    twinkl.twinkl_resource_product_line AS rpl ON re.resource_id = rpl.resource_id
+LEFT JOIN
+    twinkl.twinkl_product_line AS pl ON rpl.product_line_id = pl.id
+WHERE
+    re.`key` = 'free_logged_out' AND re.value = 1 -- free_logged_out is 1
+    AND re.resource_id NOT IN ( -- selecting resources where free_logged in is either 0 or doesn't exist
+        SELECT
+            resource_id
+        FROM
+            twinkl.twinkl_resource_extra
+        WHERE
+            `key` = 'free_logged_in' AND value = 1
+    )
+    AND dx.bundle_id IS NOT NULL -- resource is part of a bundle
+    AND r.approved = 1; -- only looking at approved resources
